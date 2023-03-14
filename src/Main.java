223237a85a27c5b8b095e377f6bfae8371032d91
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-14 23:50:28 +0300

    Написать метод, который выводит расписание на неделю. Задать на вход в метод порядковый номер дня недели и отобразить на экране то, что запланировано на этот день.
    Добавить расписание по дням недели.
    Добавить цикл для непрерывного расписания.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
+/*
+Написать метод, который выводит расписание на неделю.
+Задать на вход в метод порядковый номер дня недели и отобразить на экране то, что запланировано на этот день.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Расписание на неделю.\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер дня недели для вывода расписания");
+        int day = scanner.nextInt();
+        while (day != 0) {
+//            System.out.println("Введите номер дня недели для вывода расписания");
+//            int day = scanner.nextInt();
+            scanner.close();
+//            System.exit(0);
+            dailyPlanner(day);
+        }
+    }
+
+    static void dailyPlanner(int day) {
+        switch (day) {
+            case 1:
+                System.out.println("Понедельник\n");
+                System.out.println("6:00" + "|" + "Подъем\n");
+                break;
+            case 2:
+                System.out.println("Вторник");
+            default:
+                System.out.println("Неверный ввод");
+                break;
+        }
     }
 }
